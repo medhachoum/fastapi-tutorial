c4e802147e929a6976a2a8514d2f3aff654cc58a
medhachoum <58108126+medhachoum@users.noreply.github.com>
2024-07-17 14:19:39 +0100

    Update main.py

--- main.py
+++ main.py
@@ -17,6 +17,7 @@ app.add_middleware(
 class BMIOutput(BaseModel):
     bmi: float
     message: str
+    image: str
 
 @app.get("/")
 def Hi():
@@ -29,13 +30,17 @@ def calculate_bmi(
 ):
     bmi = weight / (height ** 2)
 
-    if bmi < 18.5:
+        if bmi < 18.5:
         message = "لديك نقص في الوزن، كُل أكثر"
+        pic = "1"
     elif 18.5 <= bmi < 25:
         message = "لديك وزن طبيعي، حافظ عليه"
+        pic = "2"
     elif 25 <= bmi < 30:
         message = "لديك زيادة في الوزن، تمرن أكثر"
+        pic = "3"
     else:
         message = "أنت تعاني من السمنة، قم بمراجعة طبيب"
+        pic="4"
 
-    return BMIOutput(bmi=bmi, message=message)
+    return BMIOutput(bmi=bmi, message=message,image=image)
